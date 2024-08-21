13beb3a656bf4272b0bec7971d126dd76575b115
Alonso Kraemer <alonso2chz@gmail.com>
2024-08-21 18:15:54 -0300

    build(Inclusao de midias e arquivos): Arquivos e midias do website

---
+++ Json/produtos.js
@@ -0,0 +1,45 @@
+export const produtos = [
+    {
+      "nome": "Areia Grossa M³ (metro)",
+      "descricao": "C&Mcimentão",
+      "preco": "R$ 165,90",
+      "imagem": "./E-CommerceRenove\Galeria\areia.png"
+    },
+    {
+      "nome": "Carrinho de Mão",
+      "descricao": "Com Caçamba Metálica Cinza 65L Tramontina",
+      "preco": "R$ 298,99",
+      "imagem": "./E-CommerceRenove\Galeria\carrinhoDeMao.png"
+    },
+    {
+      "nome": "Piso Cerâmico 'C' 43x43(cm)",
+      "descricao": "Silver Cereal",
+      "preco": "R$ 11,99",
+      "imagem": "./E-CommerceRenove\Galeria\ceramica01.png"
+    },
+    {
+      "nome": "Esfregadeira De Mão",
+      "descricao": "Tabua De Lavar Roupa Aço Galvanizado Adams",
+      "preco": "R$ 142,40",
+      "imagem": "./E-CommerceRenove\Galeria\esfregadeiraAluinio.png"
+    },
+    {
+      "nome": "Espelho Adnet Redondo",
+      "descricao": "50cm com alça e Suporte para Sala Hall Lavabo Banheiro Quarto - Outlet Dos Espelhos",
+      "preco": "R$ 63,00",
+      "imagem": "./E-CommerceRenove\Galeria\espelhoRedondo.png"
+    },
+    {
+      "nome": "Lâmpada Led Bulbo",
+      "descricao": "10w Bivolt 6.500k E27 Empalux",
+      "preco": "R$ 5,99",
+      "imagem": "./E-CommerceRenove\Galeria\lampada.png"
+    },
+    {
+      "nome": "Torneira Elétrica Zagonel Prima Touch",
+      "descricao": "Eletrônica Branca 5500w",
+      "preco": "R$ 177,60",
+      "imagem": "./E-CommerceRenove\Galeria\torneiraEletricaZagonel.png"
+    }
+  ]
+  
