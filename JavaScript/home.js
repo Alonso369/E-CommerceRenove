aa67353a5f0959709254ce88c3eac1ea93dfc356
Alonso Kraemer <alonso2chz@gmail.com>
2024-08-22 17:30:34 -0300

    mudando js1

--- JavaScript/home.js
+++ JavaScript/home.js
@@ -1,22 +1,13 @@
-// Função para carregar o JSON de produtos
-async function carregarProdutos() {
-    try {
-        const response = await fetch('produtos.json');
-        const produtos = await response.json();
-        exibirProdutos(produtos);
-    } catch (error) {
-        console.error('Erro ao carregar produtos:', error);
-    }
-}
-
+import { produtos } from "../Json/produtos";
 // Função para exibir os produtos na página
-    function exibirProdutos(produtos) {
+function exibirProdutos(produtos) {
+    try {
         const listaProdutos = document.getElementById('product-list');
-
+    
         produtos.forEach(produto => {
             const produtoCard = document.createElement('div');
             produtoCard.classList.add('product');
-
+    
             produtoCard.innerHTML = `
                 <img src="${produto.imagem}" alt="${produto.nome}">
                 <h2>${produto.nome}</h2>
@@ -24,10 +15,12 @@ async function carregarProdutos() {
                 <div class="price">${produto.preco}</div>
                 <button>Comprar</button>
             `;
-
             listaProdutos.appendChild(produtoCard);
         });
+    } catch (error) {
+        
     }
+}
 
-    // Chama a função para carregar os produtos ao iniciar a página
-    carregarProdutos();
+// Chama a função para carregar os produtos ao iniciar a página
+exibirProdutos(produtos);
