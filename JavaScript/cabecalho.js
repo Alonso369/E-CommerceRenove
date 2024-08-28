c2523f73d0579dad97a91b1af6e2de9d6e92136f
Alonso Kraemer <alonso2chz@gmail.com>
2024-08-28 12:19:13 -0300

    todos as quatro páginas com cabecalho

---
+++ JavaScript/cabecalho.js
@@ -0,0 +1,43 @@
+async function renderizarCabecalho() {
+    const cabecalhoConstainer = document.getElementById('cabecalhoContainer');
+
+    const cabecalhoCard = document.createElement('header');
+    cabecalhoCard.innerHTML = ` 
+        <header>
+        <nav class="cabeçalho">
+            <div>
+                <img class="logo" src="../Galeria/Logo.png" alt="Logo da empresa do site" title="Logo da empresa">
+            </div>
+
+            <div>
+                <ul>
+                    <li class="icone">
+                        <a href="/pages/carrinho.html"><i class="carrinho text-warning fa fa-shopping-cart"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <nav class="navbar navbar-expand-lg navbar-light bg-warning">
+            <a class="text-link-menu navbar-brand font-weight-bold" href="#">Tintas</a>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="text-link-menu nav-item nav-link active text-dark" href="/pages/home.html">Home <span class="sr-only">(Página atual)</span></a>
+                    <a class="text-link-menu nav-item nav-link" href="/pages/cadastro.html">Cadastro</a>
+                    <a class="text-link-menu nav-item nav-link" href="/pages/produto.html">Produto</a>
+                    <a class="text-link-menu nav-item nav-link disabled" href="#">Desativado</a>
+                </div>
+            </div>
+        </nav>
+    </header>
+    `;
+    //adiciona o conteúdo criado ao elemento cabeçalhoContainer
+    cabecalhoConstainer.appendChild(cabecalhoCard);
+}
+
+window.onload = async => {
+    try {
+        renderizarCabecalho();
+    } catch (error) {
+        console.error("Error ao carregar o cabeçalho.", error);
+    }
+}
